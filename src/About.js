8c44fe88ca54e036f0b0030fa9991548a973a0e1
romans1995 <cboby26@gmail.com>
2024-10-28 12:26:17 +0200

    שנםוא דקבאןםמ

--- src/About.js
+++ src/About.js
@@ -30,18 +30,22 @@ const About = (props) => {
   
   return (
     <Container id="about" className={`${classes.aboutwidth} ${classes.fadeIn}`}>
-<div className={classes.aboutLeft}><img className={classes.imag} src={profImg} alt="Roman" /></div>
+      
+      <div className={`${classes.aboutline1}`}/>
+      <div className={classes.aboutLeft}><img className={classes.imag} src={profImg} alt="Roman" />
+      </div>
       <div className={`${classes.aboutline}`}/>
+      <div className={`${classes.aboutline2}`}/>
+      
+
       <div className={classes.secondflex}>
         <h1 className={classes.title} id ="about" >About</h1>
-      <div>
+      
         <p className={classes.text}>
           Hi, I'm Roman. I'm a full stack developer with a passion for creating web applications, Since I can remember, I've been drawn to coding. At 10, I started with basic HTML and just kept going. Today, I'm a Full-Stack Developer, specializing in MongoDB and React. I excel in crafting dynamic, user-friendly websites that meet business goals and exceed user expectations. My skill set includes front-end and back-end development, ensuring cohesive and well-integrated digital experiences. I'm always ready for the next challenge.
         </p>
       </div>
- 
-
-      </div>
+      
     </Container>
   );
 };
